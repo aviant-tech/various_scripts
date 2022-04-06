e4755ca688341d75d00712840f9e67820398adc8
Herman Øie Kolden <herman@samfundet.no>
2022-04-06 15:41:12 +0200

    Add warning

--- sim-scenarios.sh
+++ sim-scenarios.sh
@@ -9,6 +9,8 @@ echo "1: Flyaway"
 read SCENARIO
 
 if [ "${SCENARIO}" = "0" ]; then
+        echo "IMPORTANT! Set the parameter NAV_TRAFF_AVOID to Disbled before proceeding. Then hit ENTER"
+        read
         venv/bin/python3 simulate_traffic.py
 elif [ "${SCENARIO}" = "1" ]; then
         venv/bin/python3 simulate_flyaway.py
